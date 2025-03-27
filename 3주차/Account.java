28a62363ffa4b04b07dabc3cdb6ac8447ea063aa
ohhyunjun <68329471+ohhyunjun@users.noreply.github.com>
2025-03-27 17:45:59 +0900

    계좌 프로그램
    상속 수업에 대한 프로젝트로 일반 계좌에게 상속 받은 3개의 다른 기능을 가진 계좌가  구현되도록 하였다.

--- 3주차/Account.java
+++ 3주차/Account.java
@@ -1,3 +1,4 @@
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
@@ -5,16 +6,16 @@ import java.util.ArrayList;
 
 public class Account {
     protected int balance = 0;
-    protected final String account_num;
-    ArrayList<String> history = new ArrayList<String>();
+    protected final String account_num;//계좌번호 설정시 다신 변경 못하도록 final로 지정.
+    ArrayList<String> history = new ArrayList<String>();// 거래내역은 지속적으로 추가되기에 ArryaList로 생성함.
     
     public Account(String account_num, int balance) {
         this.account_num = account_num;
         this.balance = balance;
-        System.out.printf("계좌를 개설하였습니다. 계좌:%s, %d원\n", this.account_num, balance);
+        System.out.printf("계좌를 개설하였습니다. \n");
     }
     
-    public void deposit(String account_num, int add_money) {
+    public void deposit(String account_num, int add_money) {//계좌 이체로 인한 이체받은 계좌측 거래내역을 저장하기 위해 만듬.
         balance += add_money;
         history.add(this.account_num + "| 입금:" + add_money + ", 총액:" + balance);
     }
@@ -38,7 +39,7 @@ public class Account {
     public void transfer(Account other_account, int tran_money) {
         if (balance >= tran_money) {
             balance -= tran_money;
-            if (other_account.account_num != null) {
+            if (other_account.account_num != null) {//null인 계좌에 이체하는것을 막기 위해 만듬.
             	other_account.deposit(this.account_num, tran_money);
                 System.out.printf("계좌: %s, 잔액: %d\n", this.account_num, balance);
                 System.out.println("송금이 성공적으로 이루어졌습니다.");
@@ -66,30 +67,77 @@ public class Account {
 class FixedDepositAccount extends Account{
 	private int regular_money;
 	public FixedDepositAccount(String account_num, int balance, int regular_money) {
-		super(account_num, balance);
+		super(account_num, balance);//부모 클래스에 선언방식을 정의 하였기에 꼭 자식클래스는 보모클래스를 먼저 호출해야함. super()를 통해
 		this.regular_money = regular_money;
-		System.out.printf("정기예금계좌를 개설하였습니다. 계좌:%s,잔액: %d원, 정기입금액: %d\n", super.account_num, super.balance, this.regular_money);
+		System.out.printf("정기예금계좌:%s,잔액: %d원, 정기입금액: %d\n", super.account_num, super.balance, this.regular_money);
 	}
 	@Override
-	public void deposit(int add_money) {
+	public void deposit(int add_money) {//일반계좌의 입금을 오버라이드함.
 		if(regular_money==add_money) {
-			super.balance+=regular_money;
-			System.out.printf("계좌: %s, 잔액: %d\n", this.account_num, balance);
+			super.balance+=regular_money; 
+			System.out.printf("정기예금계좌: %s, 잔액: %d\n", this.account_num, balance);
 	        history.add(this.account_num + "| 입금:" + add_money + ", 총액:" + balance);
 		}else {
 			System.out.println("정해진 입금액이 아닙니다.");
 		}
-    }
-	
-	
-	
+    }	
 }
-/*
+
 class AssetManagementAccount extends Account{
-	
+	private int max_money;
+	public AssetManagementAccount(String account_num, int balance, int max_money) {
+		super(account_num, balance);
+		this.max_money = max_money;
+		System.out.printf("자산관리계좌:%s,잔액: %d원, 입금한도: %d\n", super.account_num, super.balance, this.max_money);
+	}
+	@Override
+	public void deposit(int add_money) {
+		if (add_money<= max_money && add_money>0) {
+			super.balance+=add_money; 
+			System.out.printf("자산관리계좌: %s, 잔액: %d\n", this.account_num, balance);
+	        history.add(this.account_num + "| 입금:" + add_money + ", 총액:" + balance);
+		}else {
+			System.out.println("입금 한도금액보다 큽니다.");
+		}
+	}	
 }
 
 class SecuritiesDepositAccount extends Account{
+	protected final String stack_account;
+	protected int stack_balance = 0;
+	public SecuritiesDepositAccount(String account_num, int balance, String stack_account) {
+		super(account_num, balance);
+		this.stack_account = stack_account;
+		System.out.printf("일반계좌:%s,잔액: %d원/ 증권계좌:%s,잔액: %d원\n", super.account_num, super.balance, this.stack_account, this.stack_balance);
+	}
+	public void receive_securities(String account_num, int add_money) {
+		if (account_num.equals(super.account_num)) { // 계좌번호가 일반계좌일 경우.
+			super.deposit(add_money);
+		}else if (account_num.equals(this.stack_account)) {
+			this.stack_balance+=add_money;
+			System.out.printf("증권계좌: %s, 잔액: %d\n", this.stack_account, this.stack_balance);
+	        history.add(this.stack_account + "| 입금:" + add_money + ", 총액:" + this.stack_balance);
+		}
+    }
 	
+	public void transfer_securities(String account_num, Account other_account, int tran_money) {
+        if (account_num.equals(super.account_num)) { // 이체에서 일반계좌에서 이체인지 증권계좌에서 이체인지 구분하기 위해.
+        	super.transfer(other_account, tran_money);
+		}else if (account_num.equals(stack_account)) {
+			if (stack_balance >= tran_money) {
+	        	stack_balance -= tran_money;
+	            if (other_account.account_num != null) {
+	            	other_account.deposit(this.account_num, tran_money);
+	                System.out.printf("증권계좌: %s, 잔액: %d\n", this.stack_account, stack_balance);
+	                System.out.println("송금이 성공적으로 이루어졌습니다.");
+	                history.add(this.stack_account + "| 송금:" + tran_money + ", 총액:" + stack_balance);
+	                other_account.history.add(other_account.account_num + "| "+this.stack_account+"으로부터 입금:" + tran_money + ", 총액:" + other_account.balance);
+				}else {
+					System.out.println("없는 송금인입니다.");
+				}
+	        } else {
+	            System.out.println("송금액보다 잔액이 작아 송금이 불가능 합니다.");
+	        }
+		}
+    }
 }
-*/
--- 3주차/Main.java
+++ 3주차/Main.java
@@ -2,20 +2,40 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Account guest1 = new Account("123-777", 500);
-		Account guest2 = new Account("123-456", 10000);
-		guest2.deposit(15000);
-		guest2.withdraw(10000);
+		
+		Account guest1 = new Account("123-456", 0); //일반계좌로 객체 생성함.
+		Account guest2 = new Account("123-777", 500);
+		guest1.deposit(5000); // 5000원 입금함
+		guest1.withdraw(2000); // 2000원 출력함
+		guest1.transfer(guest2, 1000); // 1000원 이체함
 		System.out.println("");
+		guest1.printHistory(); // guest1 거래내역 출력
 		guest2.printHistory();
 		System.out.println("");
-		guest2.transfer(guest1, 5000);
+		
+		FixedDepositAccount guest3 = new FixedDepositAccount("712-449", 5000, 5000);
+		//정기예금계좌 객체 생성 (계좌번호, 초기금액, 정기입금금액)
+		guest3.deposit(5000);
+		guest3.deposit(3000);
 		System.out.println("");
-		guest1.printHistory();
+		guest3.printHistory();
 		System.out.println("");
 		
-		FixedDepositAccount guest3 = new FixedDepositAccount("111-765", 50000, 5000);
-		guest3.deposit(3000);
+		AssetManagementAccount guest4 = new AssetManagementAccount("123-123", 0, 10000);
+		//자산관리계좌 객체 생성 (계좌번호, 초기금액, 상한선금액)
+		guest4.deposit(8000); 
+		guest4.deposit(12000);
+		System.out.println("");
+		guest4.printHistory();
+		System.out.println("");
+		
+		SecuritiesDepositAccount  guest5 = new SecuritiesDepositAccount("724-659", 5700, "144-725");
+		//증권예탁계좌 객체 생성 (계좌번호, 초기금액, 증권계좌번호). 증권게좌는 초기금액 0으로 하였음.
+		guest5.receive_securities("144-725", 2000);
+		guest5.transfer_securities("144-725", guest4, 1000);
+		System.out.println("");
+		guest5.printHistory();
+		guest4.printHistory();
 		
 	}
 
